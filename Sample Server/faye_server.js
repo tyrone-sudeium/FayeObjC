3f42a2ecbc4aa457574abd056f9fc839eb58a92e
Tyrone Trevorrow <tyrone@sudeium.com>
2013-02-17 15:44:18 +1100

    Updated sample server to use NPM.

--- Sample Server/faye_server.js
+++ Sample Server/faye_server.js
@@ -1,7 +1,5 @@
-require.paths.unshift(__dirname + "/vendor");
-
 var http = require('http'),
-    faye = require('faye-node');
+    faye = require('faye');
 
 var bayeux = new faye.NodeAdapter({
   mount:    '/faye',
